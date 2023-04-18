59bfd744d25ac2685c553dd0c5e30671efda59e7
frensing <dev@frensing.de>
2023-04-18 18:58:57 +0000

    4.18 - 4.20

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -11,7 +12,12 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -3,6 +3,14 @@ const morgan = require('morgan')
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
+const tokenExtractor = (request, res, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unkown endpoint' })
 }
@@ -14,6 +22,10 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(400).json({ error: error.message })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({ error: error.message })
   }
 
   next(error)
@@ -21,6 +33,7 @@ const errorHandler = (error, req, res, next) => {
 
 module.exports = {
   morgan,
+  tokenExtractor,
   unknownEndpoint,
   errorHandler
 }
