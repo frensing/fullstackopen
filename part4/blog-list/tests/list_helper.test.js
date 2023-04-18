91168107a55260c67fae9622fa79be0d023efd1a
frensing <dev@frensing.de>
2023-04-18 07:58:38 +0000

    4.5

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -47,6 +47,61 @@ describe('total likes', () => {
   })
 })
 
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithThreeBlogs = [
+    ...listWithOneBlog,
+    {
+      _id: '643e3dbaa585a48c7753f257',
+      title: 'Test Blog',
+      author: 'Test Author',
+      url: 'http://www.test.com',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '643e3dbaa585a48c7753f258',
+      title: 'Test Blog2',
+      author: 'Test Author2',
+      url: 'http://www.test2.com',
+      likes: 1,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, this is favorite', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    })
+  })
+
+  test('when list has no blogs, returns empty object', () => {
+    expect(listHelper.favoriteBlog([])).toEqual({})
+  })
+
+  test('list with 3 blogs, finds favorite', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlogs)
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    })
+  })
+})
+
 
 test('dummy returns one', () => {
   const blogs = []
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -9,4 +9,19 @@ const totalLikes = (blogs) => {
   return likes.reduce(reducer, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+
+  const maxLikes = Math.max(...blogs.map(b => b.likes))
+
+  const favBlog = blogs.filter(b => b.likes === maxLikes)[0]
+  return {
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
