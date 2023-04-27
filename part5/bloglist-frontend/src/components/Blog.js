3ade72dc5f112a010c656ed711d3a4c42de38831
frensing <dev@frensing.de>
2023-04-27 14:18:09 +0000

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,31 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+import Togglable from "./Togglable"
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+  
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+      { !visible ?
+        <button onClick={() => setVisible(!visible)}>view</button> :
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>like</button></div>
+          <div>{blog.user.name}</div>
+        </div>}
+    </div>
+  )
+}
 
 export default Blog
