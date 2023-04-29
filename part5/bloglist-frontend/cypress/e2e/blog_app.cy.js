9a6c126283e40085a15f5abb60a263f2c22cbc09
frensing <dev@frensing.de>
2023-04-29 13:18:51 +0300

    5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,11 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      'username': 'testuser',
+      'name': 'Test User',
+      'password': 'password'
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +13,25 @@ describe('Blog app', function() {
     cy.contains('log in to application')
     cy.contains('username:')
     cy.contains('password:')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('password')
+      cy.get('#loginBtn').click()
+
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('wrong')
+      cy.get('#loginBtn').click()
+
+      cy.contains('Wrong credentials').should('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Test User logged in')
+    })
   })
 })
