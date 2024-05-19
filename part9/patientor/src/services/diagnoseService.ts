4d3c2e42cea97a942f958fd006b0785d1e2cd3d6
frensing <dev@frensing.de>
2024-05-19 11:42:40 +0000

    9.10

---
+++ part9/patientor/src/services/diagnoseService.ts
@@ -0,0 +1,10 @@
+import diagnoseData from "../../data/diagnoses";
+import { Diagnosis } from "../types";
+
+const getEntries = (): Diagnosis[] => {
+  return diagnoseData;
+};
+
+export default {
+  getEntries,
+};
